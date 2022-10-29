d9d22f21987eb6889c4f7f3be4d210918095acbd
Renan Fernandes <renzinestuods@gmail.com>
2022-10-29 11:41:58 -0300

    :sparkles: feat: adiciona status de usuário

--- desafio1.sql
+++ desafio1.sql
@@ -13,6 +13,7 @@ DROP DATABASE IF EXISTS SpotifyClone;
       `name` VARCHAR(45) NOT NULL,
       `age` INT,
       `plan` INT,
+      `status` INT,
       `signature_date` DATE NOT NULL,
       FOREIGN KEY (plan) REFERENCES SpotifyClone.plans (plan_id)
   ) engine = InnoDB;
@@ -62,18 +63,18 @@ DROP DATABASE IF EXISTS SpotifyClone;
     (3, 'pessoal', 6.99),
     (4, 'familiar', 7.99);
 
-  INSERT INTO SpotifyClone.users (user_id, name, age, plan, signature_date)
+  INSERT INTO SpotifyClone.users (user_id, name, age, plan, signature_date, status)
   VALUES
-    (1, 'Barbara Liskov', 82, 1, '2019-10-20'),
-    (2, 'Robert Cecil Martin', 58, 1, '2017-01-06'),
-    (3, 'Ada Lovelace', 37, 4, '2017-12-30'),
-    (4, 'Martin Fowler', 46, 4, '2017-01-17'),
-    (5, 'Sandi Metz', 58, 4, '2018-04-29'),
-    (6, 'Paulo Freire', 19, 2, '2018-02-14'),
-    (7, 'Bell Hooks', 26, 2, '2018-01-05'),
-    (8, 'Christopher Alexander', 85, 3, '2019-06-05'),
-    (9, 'Judith Butler', 45, 3, '2020-05-13'),
-    (10, 'Jorge Amado', 58, 3, '2017-02-17');
+    (1, 'Barbara Liskov', 82, 1, '2019-10-20', 1),
+    (2, 'Robert Cecil Martin', 58, 1, '2017-01-06', 1),
+    (3, 'Ada Lovelace', 37, 4, '2017-12-30', 0),
+    (4, 'Martin Fowler', 46, 4, '2017-01-17', 1),
+    (5, 'Sandi Metz', 58, 4, '2018-04-29', 1),
+    (6, 'Paulo Freire', 19, 2, '2018-02-14', 0),
+    (7, 'Bell Hooks', 26, 2, '2018-01-05', 0),
+    (8, 'Christopher Alexander', 85, 3, '2019-06-05', 0),
+    (9, 'Judith Butler', 45, 3, '2020-05-13', 1),
+    (10, 'Jorge Amado', 58, 3, '2017-02-17', 0);
 
   INSERT INTO SpotifyClone.artists (artist_id, name)
   VALUES
