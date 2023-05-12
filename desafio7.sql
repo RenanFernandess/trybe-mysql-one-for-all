78081ce1cd06f6b493d11d362af4d0be352153d9
Renan Fernandes <renzinestuods@gmail.com>
2023-05-12 19:28:29 -0300

    :sparkles: feat: Mostra uma relação de todos os álbuns produzidos por cada pessoa artista, ordenada pela quantidade de seguidores que ela possui

--- desafio7.sql
+++ desafio7.sql
@@ -1,37 +1,12 @@
--- Active: 1666720144667@@127.0.0.1@3307@SpotifyClone
+
 SELECT
-  albums.name AS 'album',
-  COUNT(following.user) AS 'seguidores'
+	artists.name AS 'artista',
+	albums.name AS 'album',
+  COUNT(follows.artist) AS 'seguidores'
 FROM SpotifyClone.albums AS albums
-INNER JOIN SpotifyClone.following_artists AS following
-ON albums.artist = following.artist
-GROUP BY album;
-
-SELECT
-  artists.name AS 'artista',
-  DISTINCT albums.name AS 'album',
-  COUNT(following.user) AS 'seguidores'
-FROM `SpotifyClone`.artists AS artists
-INNER JOIN `SpotifyClone`.albums AS albums
-ON artists.artist_id = albums.artist
-INNER JOIN SpotifyClone.following_artists AS following
-ON artists.artist_id = following.artist
-GROUP BY album;
-
-
-SELECT
-  name
-FROM `SpotifyClone`.artists AS artists
-INNER JOIN SpotifyClone.following_artists AS following
-ON artists.artist_id = following.artist;
-
-
-SELECT
-  artists.name AS 'artista',
-  albums.name AS 'album',
-  follows.artist AS 'seguidores'
-FROM SpotifyClone.artists AS artists
-INNER JOIN SpotifyClone.albums AS albums
-ON artists.artist_id = albums.artist
-LEFT JOIN SpotifyClone.following_artists AS follows
-ON artists.artist_id = follows.artist;
+INNER JOIN SpotifyClone.artists AS artists
+ON albums.artist = artists.artist_id
+INNER JOIN SpotifyClone.following_artists AS follows
+ON albums.artist = follows.artist
+GROUP BY albums.album_id
+ORDER BY seguidores DESC, artista, album;
